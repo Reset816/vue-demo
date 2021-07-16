a570c925495cb9f4202cccb663966071388644d6
wht <2986978254@qq.com>
2021-07-16 15:59:10 +0800

    add info view

--- src/router/index.js
+++ src/router/index.js
@@ -20,11 +20,18 @@ const routes = [{
         path: "search",
         name: "search",
         component: () => import( /* webpackChunkName: 'main' */ '@/views/main/Search.vue')
+<<<<<<< Updated upstream
     },
     {
         path: "search-engine",
         name: "search-engine",
         component: () => import( /* webpackChunkName: 'main' */ '@/views/main/Search-engine.vue')
+=======
+    },{
+        path: "info",
+        name: "info",
+        component: () => import( /* webpackChunkName: 'main' */ '@/views/main/Info.vue')
+>>>>>>> Stashed changes
     }]
 },
 {
